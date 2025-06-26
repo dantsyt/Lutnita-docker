f7ddb29fb1252e2f592b516fea6e694eb1e6d917
dantsyt <dantsyt@gmail.com>
2025-06-27 00:39:01 +0300

    add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,15 @@ FROM alpine:3.22.0
 
 LABEL maintainer="dantsyt@gmail.com"
 
+ENV MONGODB_URI="mongodb://mongodb:27017/lutnita"
+
 COPY . /lutnita-app
 
 WORKDIR /lutnita-app
 
 RUN apk add --update nodejs npm && npm install && npm audit fix
 
-EXPOSE 8080
+EXPOSE 3000
 
 ENTRYPOINT ["node", "./src/app.js"]
 
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const logger = require('../logger')
 
 mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((err) => {
+        logger.error('MongoDB connection error:', err)
+    })
